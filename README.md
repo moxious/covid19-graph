ac38e735e5e9aa43f4ac266a820fe54c6225ba1d
Moxious <moxious@oldhat.org>
2020-03-04 07:55:50 -0500

    load all

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Using a forked copy of Johns Hopkins data to load trends on COVID-19 infections 
 Check the `import-all.sh` script.  Adjust particulars for your situation, and then run it to re-create a database
 from scratch.
 
+```
+./import-all.sh
+```
+
+This is a fast moving situation, so make sure to check the dates in import-all of the list of dates that
+you want to import.  I've pointed this at a forked copy of the JHU data so that it can't go missing,
+but this necessitates keeping the fork in sync.
+
+
+
--- import-all.sh
+++ import-all.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-ENDPOINT=bolt://localhost
-USERNAME=neo4j
-PASSWORD=admin
+DEFAULT_ENDPOINT=bolt://localhost:7687
+ENDPOINT=${NEO4J_URI:-$DEFAULT_ENDPOINT}
+USERNAME=${NEO4J_USERNAME:-neo4j}
+PASSWORD=${NEO4J_PASSWORD:-admin}
 
-cypher="/Users/davidallen/Library/Application Support/Neo4j Desktop/Application/neo4jDatabases/database-8bf32f49-6a41-4b02-843e-dbdb50de760d/installation-4.0.1/bin/cypher-shell"
+cypher_shell="/Users/davidallen/Library/Application Support/Neo4j Desktop/Application/neo4jDatabases/database-8bf32f49-6a41-4b02-843e-dbdb50de760d/installation-4.0.1/bin/cypher-shell"
 
+function run_cypher {
+    echo "Running $1"
+    echo "$1" | "$cypher_shell" -u "$USERNAME" -a "$ENDPOINT" -p "$PASSWORD"
+}
 
+# Set up the database.
+run_cypher "00-indexes.cypher"
+run_cypher "01-load-world-cities.cypher"
+run_cypher "02-administrative-units.cypher"
+run_cypher "03-country-bounding-boxes.cypher"
 
-# Load each date.
-for dt in  "03-03-2020" "03-02-2020" "03-01-2020" "02-29-2020" ; do
+# Load each date of COVID data.
+for dt in "03-03-2020" "03-02-2020" "03-01-2020" "02-29-2020" ; do
  export report_date=$dt
- echo "Loading $file"
- cat 04-covid19.cypher | envsubst | "$cypher" -a $ENDPOINT -u $USERNAME -p $PASSWORD
+ echo "Loading COVID stats for $dt"
+ cat 04-covid19.cypher | envsubst | "$cypher_shell" -a $ENDPOINT -u $USERNAME -p $PASSWORD
 
  if [ $? -ne 0 ] ; then
     exit 1
